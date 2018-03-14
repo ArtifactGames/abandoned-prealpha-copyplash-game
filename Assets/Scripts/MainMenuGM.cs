166532089fe1cffe32d2c94e8876c8669be8bca6
Andrés Vieira <anvieiravazquez@gmail.com>
2018-03-14 18:15:56 +0100

    Adds menu handlers, animations and scene layouts

---
+++ Assets/Scripts/MainMenuGM.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuGM : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void newGame(){
+		SceneManager.LoadScene("game-session");
+	}
+
+	public void exit(){
+		Application.Quit();
+	}
+}
