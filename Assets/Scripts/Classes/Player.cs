61a3268916d2701d16774eaf553ebfd8251b68a2
MarioBlanco <mblanco@gradiant.org>
2018-03-30 18:41:46 +0200

    Improved the Player declaration by defining a constructor in playerlist. Also fixed a bug with the UI to update de playerlist

--- Assets/Scripts/Classes/Player.cs
+++ Assets/Scripts/Classes/Player.cs
@@ -6,27 +6,32 @@ using UnityEngine;
 
 [Serializable]
 public class Player
-{   
-    public string id;
-    public string nick;
+{
+	public string id;
+	public string nick;
 
-    public static string ListToString(List<Player> p)
-    {
-        string toRet = "";
-        foreach (Player player in p)
-        {
-            toRet += string.Format("{0} \n", player.nick);
-        }
-        return toRet;
-    }
+	public static string ListToString (List<Player> p)
+	{
+		string toRet = "";
+		foreach (Player player in p) {
+			toRet += string.Format ("{0} \n", player.nick);
+		}
+		return toRet;
+	}
 }
 
 [Serializable]
 public class PlayerList
 {
-    public Player[] players;
+	public List<Player> players;
 
-    public static List<Player> AsList(PlayerList playerList) {
-        return new List<Player>(playerList.players);
-    }
+	public PlayerList ()
+	{
+		this.players = new List<Player> ();
+	}
+
+	public static List<Player> AsList (PlayerList playerList)
+	{
+		return new List<Player> (playerList.players);
+	}
 }
--- Assets/Scripts/Components/GameLobbyGM.cs
+++ Assets/Scripts/Components/GameLobbyGM.cs
@@ -8,88 +8,99 @@ using UnityEngine.UI;
 public class GameLobbyGM : MonoBehaviour
 {
 
-    public Text passwordText;
-    public Text playerListText;
+	public Text passwordText;
+	public Text playerListText;
+	private String newPlayerList;
 
-    // Use this for initialization
-    void Start()
-    {
-        try
-        {
-            passwordText.text = GameInfo.lobby.password.ToString("D4");
-        }
-        catch (NullReferenceException)
-        {
-            passwordText.text = "ERROR";
-        }
+	// Use this for initialization
+	void Start ()
+	{
+		newPlayerList = "";
+		try {
+			playerListText.text = "None";
+			passwordText.text = GameInfo.lobby.password.ToString ("D4");
+		} catch (NullReferenceException) {
+			passwordText.text = "ERROR";
+		}
 
-        ServerManager.EstablishConnection();
-        ServerManager.ws.OnMessage += this.OnReceiveResponse;
+		ServerManager.EstablishConnection ();
+		ServerManager.ws.OnMessage += this.OnReceiveResponse;
 
-    }
+	}
 
-    private void OnReceiveResponse(object sender, WebSocketSharp.MessageEventArgs e)
-    {
-        // TODO: Handle players joining the lobby.
+	private void OnReceiveResponse (object sender, WebSocketSharp.MessageEventArgs e)
+	{
+        
 
-        CommandResponse c;
+		CommandResponse c;
 
-        try
-        {
-            c = JsonUtility.FromJson<CommandResponse>(e.Data);
-        }
-        catch (NullReferenceException)
-        {
-            Debug.LogWarning("Wrong Command Response serialization");
-            c = null;
-        }
+		try {
+			c = JsonUtility.FromJson<CommandResponse> (e.Data);
+		} catch (NullReferenceException) {
+			Debug.LogWarning ("Wrong Command Response serialization");
+			c = null;
+		}
 
-        if (c == null)
-        {
-            return;
-        }
+		if (c == null) {
+			return;
+		}
 
-        switch (c.action)
-        {
-            case CommandAction.UPDATE_PLAYERS:
-                this.OnUpdatePlayerCommandAction(c);
-                break;
-            case CommandAction.START_GAME:
-                break;
-            default:
-                Debug.LogError("NOT IMPLEMENTED: CommandAction not implemented");
-                break;
-        }
+		switch (c.action) {
+		case CommandAction.UPDATE_PLAYERS:
+			this.OnUpdatePlayerCommandAction (c);
+			break;
+		case CommandAction.START_GAME:
+			this.OnStartGameCommandAction (c);
+			break;
+		default:
+			Debug.LogError ("NOT IMPLEMENTED: CommandAction not implemented");
+			break;
+		}
 
         
-    }
+	}
 
-    private void OnStartGameCommandAction(CommandResponse c){
-        SceneManager.LoadScene("first-round-explanation");
-    }
+	private void OnStartGameCommandAction (CommandResponse c)
+	{
+		SceneManager.LoadScene ("first-round-explanation");
+	}
 
-    private void OnUpdatePlayerCommandAction(CommandResponse c){
-        try
-        {
-            PlayerList playerList = JsonUtility.FromJson<PlayerList>(c.payload);
-            UpdatePlayerList(PlayerList.AsList(playerList));
-        }
-        catch (NullReferenceException)
-        {
-            Debug.LogWarning("Wrong Player payload serialization");
-        }
-        catch (UnityException err)
-        {
-            Debug.LogError(err);
-        }
-    }
+	private void OnUpdatePlayerCommandAction (CommandResponse c)
+	{
+		try {
+			PlayerList playerList = JsonUtility.FromJson<PlayerList> (c.payload);
+		
+			UpdatePlayerList (PlayerList.AsList (playerList));
 
-    private void UpdatePlayerList(List<Player> p)
-    {
-        // Update internal lobby player list
-        GameInfo.lobby.players = p;
+		} catch (NullReferenceException err) {
+			newPlayerList = "";
+			Debug.LogWarning ("Wrong Player payload serialization or other related nullpoint @ UpdatePlayerList:" + err);
+		} catch (UnityException err) {
+			Debug.LogError (err);
+		}
+	}
 
-        // Update player list text
-        playerListText.text = Player.ListToString(GameInfo.lobby.players);
-    }
+	private void UpdatePlayerList (List<Player> p)
+	{
+
+		// Update internal lobby player list
+		if (GameInfo.lobby.players == null) {
+			GameInfo.lobby.players = new List<Player> ();
+		}
+		if (p != null) {
+			GameInfo.lobby.players = p;
+			newPlayerList = Player.ListToString (GameInfo.lobby.players);
+		}
+		
+	}
+
+	void Update ()
+	{
+		// Update player list text
+		if (newPlayerList != "") {
+			Debug.Log (newPlayerList);
+			playerListText.text = newPlayerList;
+			newPlayerList = "";
+		}
+	}
 }
