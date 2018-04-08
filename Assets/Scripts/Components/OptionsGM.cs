aaef0be1f701f63c236965c57c090371efe03d55
Andrés Vieira <anvieiravazquez@gmail.com>
2018-04-08 20:53:51 +0200

    Adds on the fly changes to the options menu

--- Assets/Scripts/Components/OptionsGM.cs
+++ Assets/Scripts/Components/OptionsGM.cs
@@ -4,24 +4,40 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class OptionsGM : MonoBehaviour {
+public class OptionsGM : MonoBehaviour
+{
 
-	public Toggle fullscreen;
-	public Dropdown gameLanguage;
+    public Toggle fullscreen;
+    public Dropdown gameLanguage;
 
-	// Use this for initialization
-	void Start () {
-		fullscreen.isOn = GameConfig.fullscreen;
-		gameLanguage.value = GameConfig.locale;
-	}
-	
-	public void onSave(){
-		GameConfig.fullscreen = fullscreen.isOn;
-		GameConfig.locale = gameLanguage.value;
+    // Use this for initialization
+    void Start()
+    {
+        fullscreen.isOn = GameConfig.fullscreen;
+        gameLanguage.value = GameConfig.locale;
+    }
+
+    public void onSave()
+    {
+        GameConfig.fullscreen = fullscreen.isOn;
+        GameConfig.locale = gameLanguage.value;
+		this.OptionsApply();
         SceneManager.LoadScene("main-menu");
     }
 
-	public void onCancel(){
-		SceneManager.LoadScene("main-menu");
-	}
+    public void onCancel()
+    {
+        SceneManager.LoadScene("main-menu");
+    }
+
+    /// <summary>
+    /// Applies the different options on the fly, if possible.
+    /// </summary>
+    public void OptionsApply()
+    {
+        if (Screen.fullScreen != GameConfig.fullscreen)
+        {
+            Screen.fullScreen = GameConfig.fullscreen;
+        }
+    }
 }
