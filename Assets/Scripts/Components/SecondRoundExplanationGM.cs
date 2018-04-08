3f91e6b2b66e0ac4af862873fc056db62aebb696
Andrés Vieira <anvieiravazquez@gmail.com>
2018-04-08 18:44:59 +0200

    Adds second round scenes for explanation and waiting
    Related with https://trello.com/c/b2WMBp1r

---
+++ Assets/Scripts/Components/SecondRoundExplanationGM.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SecondRoundExplanationGM : MonoBehaviour {
+
+	public void GoToSecondRoundWait(){
+		SceneManager.LoadScene("second-round-wait");
+	}
+}
---
+++ Assets/Scripts/Components/SecondRoundWaitGM.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondRoundWaitGM : MonoBehaviour
+{
+
+    void Start()
+    {
+        ServerManager.ws.OnMessage += this.OnRecieveResponse;
+    }
+
+    private void OnRecieveResponse(object sender, WebSocketSharp.MessageEventArgs e)
+    {
+        CommandResponse c;
+
+        try
+        {
+            c = JsonUtility.FromJson<CommandResponse>(e.Data);
+        }
+        catch (NullReferenceException)
+        {
+            Debug.LogWarning("Wrong Command Response serialization");
+            c = null;
+        }
+
+        if (c == null)
+        {
+            return;
+        }
+
+		// TODO: Manage server response
+		Debug.Log(c);
+    }
+}
